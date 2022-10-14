54897fb961ede84fc53aaf524d157c1609f6a1e7
carola <carolacollao@gmail.com>
2022-10-13 23:10:04 -0300

    formato card

--- src/views/Detalles.jsx
+++ src/views/Detalles.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useContext } from "react";
 import { useParams } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+
 
 import ContextAPI from "../ContextAPI ";
 
@@ -10,10 +10,10 @@ const Detalles = () => {
   const { id } = useParams();
 
 
-  const { pizzas} = useContext(ContextAPI)
+  const { pizzas } = useContext(ContextAPI)
 
- const elemento = pizzas.filter((e) => e.id === id)
- console.log(elemento)
+  const elemento = pizzas.filter((e) => e.id === id)
+  console.log(elemento)
 
   /*const seleccionarPizza = ({ id, price, img, name }) => {
     const productoEncontradoIndex = pizzas.findIndex((e) => e.id === id);
@@ -26,20 +26,27 @@ const Detalles = () => {
 
   return (
 
-    <div>Detalle
-      <div className=' container my-5'>
-        <div className='row'>
+    <div className="container m-4 mx-auto">
+      <div class="card flex-row row-cols-md-auto">
 
-          <p>{pizzas.name}</p>
+        <img class="card-img-left example-card-img-responsive" alt={elemento[0].name}
+          src={elemento[0].img} /> 
 
-          <Card className='my-2 mb-2 text-center' border="primary" style={{ width: 'auto', margin: 'auto' }}>
-            <Card.Body>
-              <Card.Title>{id}</Card.Title>
-            </Card.Body>
-          </Card>
+        <div class="card-body">
+          <div class="card-title h5 h4-sm text-start"><h2>{elemento[0].name}</h2></div>
+          <p class="card-text text-start">{elemento[0].desc}</p>
 
-          <p>{elemento[0].name}</p>
-        
+          <ul className="text-start border-top ">
+            <h4 className="text-secondary text-start m-4">Ingredientes:</h4>
+            {elemento[0].ingredients.map((i) => (
+              <li className="list-group-item">🍕  {i}</li>
+            ))}
+          </ul>
+
+          <div className="row">
+            <h4 className="text-danger"> ${elemento[0].price}</h4>
+            <button className="btn btn-danger m-2" onClick={() => console.log('aquí va el carrito de compras')}>Añadir 🛒</button>
+          </div>
         </div>
       </div>
 
